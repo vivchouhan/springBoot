6c84961d97a0bed3cb1eba5f135fe53a7fc98d2a
vivek <vivek.chouhan70@gmail.com>
2020-06-10 17:02:55 +0530

    initial commit

---
+++ SpringSe/src/main/java/com/telusko/demo/ExampleClass.java
@@ -0,0 +1,10 @@
+package com.telusko.demo;
+
+public class ExampleClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is created for exmaple purpose");
+		System.out.println("this is created for exmaple purpose");
+	}
+}
---
+++ SpringSe/src/main/java/com/telusko/demo/gitsecreate.java
@@ -0,0 +1,12 @@
+package com.telusko.demo;
+
+public class gitsecreate{
+
+public static void main(String args[]) 
+{
+	System.out.println("this is first employee");
+System.out.println("git se create ka example this is done by another employee");
+System.out.println("commit");
+
+}
+}
